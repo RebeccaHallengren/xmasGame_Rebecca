d5069f101ca841a6a422a48ad271823c45ce6a53
Rebecca <rebecca_hallengren@hotmail.com>
2023-11-02 16:32:08 +0100

    add array pages

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  startTheGame();
-  //changeMisson ();
-  //decorateTree();
+  moveOn();
 }
 
-function startTheGame() {
-  const text = document.querySelector(".introHeader .introDescription");
+function moveOn() {
+  const header = document.querySelector(".introHeader");
+  const text = document.querySelector(".introDescription");
+  const Button1 = document.querySelector("#option0");
+  const Button2 = document.querySelector("#option1");
+  const Button3 = document.querySelector("#option2");
+  const body = document.getElementById("body");
+
+  const page = pages[activepage]; //Pages ligger i missions, och active page är nu 0.
+
+  header.textContent = page.Headers;
+  text.textContent = page.Text;
+  Button1.textContent = page.Btn1.Text;
+  Button2.textContent = page.Btn2.Text;
+  Button3.textContent = page.Btn3.Text;
+  body.style.backgroundImage = page.backgroundImage;
+
+  Button1.onclick = function () {
+    goToNextpage(page.Btn1.nextPage);
+  };
+
+  Button2.onclick = function () {
+    goToNextpage(page.Btn2.nextPage);
+  };
+
+  Button3.onclick = function () {
+    goToNextpage(page.Btn3.nextPage);
+  };
+}
+
+function goToNextpage(pageIndex) {
+  activepage = pageIndex;
+  moveOn();
+}
+//Knapp för att spara användarens namn.
+function addName() {
   const button = document.getElementById("introBtn");
 
-  //Knapp för att spara användarens namn.
   button.onclick = function () {
     const input = document.getElementById("stringName");
     const name = document.querySelector(".name");
@@ -18,7 +49,3 @@ function startTheGame() {
     name.innerHTML = "Tomtenissen " + stringName.value + " räddar julen!";
   };
 }
-
-const startGameBtn = document.getElementById("").toString();
-
-let activemission = 0;
---
+++ missons.js
@@ -0,0 +1,70 @@
+let activepage = 0;
+
+const pages = [
+  {
+    Headers: "Uppdrag 1",
+    Text: "Om man inte skickat har, får man oftast det som blev kvar. Milo har glömt skicka sin önskemlista till tomten i år, det skulle han gjort i förrgår. Hjälp Milo så listan tomten når, vilket sätt du föreslår?",
+    Btn1: {
+      Text: "Slangbella",
+      nextPage: 1,
+    },
+    Btn2: {
+      Text: "Brevduva",
+      nextPage: 1,
+    },
+    Btn3: {
+      Text: "Tomtens släde",
+      nextPage: 1,
+    },
+    backgroundImage: 'URL("Items/BackgroundMission1.png")', //Backgrundsbilden för första uppdraget
+  },
+  {
+    Headers: "Uppdrag 2",
+    Text: "En bit in i december vi är, och tomten har precis mottagit ditt kuvert. Vi står nu utanför hans hus, vi i luften känna det på pepparkakor bjuds. Vi mot hans renar ler, som visar oss runt i detta vinterkvarter. I gengäld vi tillbaka ge, morötter, glass eller kycklingfilé.",
+    Btn1: {
+      Text: "Morötter",
+      nextPage: 2,
+    },
+    Btn2: {
+      Text: "Glass",
+      nextPage: 2,
+    },
+    Btn3: {
+      Text: "Kycklingfilé",
+      nextPage: 2,
+    },
+    backgroundImage: 'URL("Items/BackgroundMission2.png")',
+  },
+  {
+    Headers: "Uppdrag 3",
+    Text: "Hej tomtegubbar, tänd ett ljus och mer jul, att sjunga sånger tycker kören är jättekul! En jultext dem är osäkra på, kan du hjälpa dem att texten återfå",
+    Btn1: {
+      Text: "??",
+      nextPage: 3,
+    },
+    Btn2: {
+      Text: "??",
+      nextPage: 3,
+    },
+    Btn3: {
+      Text: "??",
+      nextPage: 3,
+    },
+  },
+  {
+    Headers: "Uppdrag 4",
+    Text: "Med snö du kan skapa många ting, du kan med tillbehör sedan göra iordning. Filippa, Valter och Agnes sin snögrubbe rullat, ",
+    Btn1: {
+      Text: "??",
+      nextPage: 4,
+    },
+    Btn2: {
+      Text: "??",
+      nextPage: 4,
+    },
+    Btn3: {
+      Text: "??",
+      nextPage: 4,
+    },
+  },
+];
