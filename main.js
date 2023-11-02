2477618efaefdd08c402b003298c3a53ce14340f
Rebecca <rebecca_hallengren@hotmail.com>
2023-11-02 12:01:15 +0100

    addButtonFirstPage

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 window.addEventListener("DOMContentLoaded", main);
 
-function main() {}
+function main() {
+  startTheGame();
+  //changeMisson ();
+  //decorateTree();
+}
+
+function startTheGame() {
+  const text = document.querySelector(".introHeader .introDescription");
+  const button = document.getElementById("introBtn");
+
+  //Knapp för att spara användarens namn.
+  button.onclick = function () {
+    const input = document.getElementById("stringName");
+    const name = document.querySelector(".name");
+
+    name.innerHTML = "Tomtenissen " + stringName.value + " räddar julen!";
+  };
+}
+
+const startGameBtn = document.getElementById("").toString();
+
+let activemission = 0;
