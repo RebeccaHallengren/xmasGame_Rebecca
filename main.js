dea20de7abc7d2e488c59eec0a4053088b18369c
Rebecca <rebecca_hallengren@hotmail.com>
2023-11-08 08:48:38 +0100

    add boolean on buttons

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
+  startGame();
   moveOn();
 }
 
+function startGame() {
+  const startHeader = document.querySelector(".introHeader");
+  const startText = document.querySelector(".introDescription");
+  const startInput = document.querySelector(".inputName");
+  const startButton = document.querySelector("#introBtn");
+
+  startHeader.textContent = firstPage.HeaderStart;
+  startText.textContent = firstPage.TextStart;
+  startInput.textContent = firstPage.InputStart;
+  startButton.textContent = firstPage.BtnStart.Text;
+
+  startButton.addEventListener("click", function () {
+    addName();
+  });
+}
+
 function moveOn() {
-  const header = document.querySelector(".introHeader");
-  const text = document.querySelector(".introDescription");
+  const header = document.querySelector(".missionHeader");
+  const text = document.querySelector(".missionDescription");
   const Button1 = document.querySelector("#option0");
   const Button2 = document.querySelector("#option1");
   const Button3 = document.querySelector("#option2");
+  const Item1 = document.getElementsByClassName("item0"); //INVENTORY?
   const body = document.getElementById("body");
 
   const page = pages[activepage]; //Pages ligger i missions, och active page är nu 0.
@@ -19,32 +37,57 @@ function moveOn() {
   Button1.textContent = page.Btn1.Text;
   Button2.textContent = page.Btn2.Text;
   Button3.textContent = page.Btn3.Text;
+
   body.style.backgroundImage = page.backgroundImage;
 
   Button1.onclick = function () {
-    goToNextpage(page.Btn1.nextPage);
+    checkAnswer(page.Btn1.isCorrect, page.Item1, page.Btn1.nextPage); //true/false
+    // goToNextpage(page.Btn1.nextPage);
   };
 
   Button2.onclick = function () {
-    goToNextpage(page.Btn2.nextPage);
+    checkAnswer(page.Btn2.isCorrect, page.Item1, page.Btn2.nextPage);
+    // goToNextpage(page.Btn2.nextPage);
   };
 
   Button3.onclick = function () {
-    goToNextpage(page.Btn3.nextPage);
+    checkAnswer(page.Btn3.isCorrect, page.Item1, page.Btn3.nextPage);
+    // goToNextpage(page.Btn3.nextPage);
   };
+}
+
+function checkAnswer(isCorrect, item, nextPage) {
+  if (isCorrect === true) {
+    inventory.push(item);
+    showMessageBox(nextPage);
+  } else if (isCorrect === false) {
+    goToNextpage(nextPage);
+  }
+}
+
+function showMessageBox(nextPage) {
+  const rightAnswer = document.querySelector(".rightAnswer");
+
+  rightAnswer.style.display = "block";
+
+  setTimeout(function () {
+    rightAnswer.style.display = "none";
+    goToNextpage(nextPage);
+  }, 3000);
 }
 
 function goToNextpage(pageIndex) {
   activepage = pageIndex;
   moveOn();
 }
-//Knapp för att spara användarens namn.
+
+//Till förstasidan.
+
 function addName() {
   const button = document.getElementById("introBtn");
-
   button.onclick = function () {
     const input = document.getElementById("stringName");
-    const name = document.querySelector(".name");
+    const name = document.getElementById("name");
 
     name.innerHTML = "Tomtenissen " + stringName.value + " räddar julen!";
   };
--- missons.js
+++ missons.js
@@ -1,3 +1,18 @@
+//Obejkt för startsida
+const firstPage = {
+  HeaderStart: "Rädda Julen!",
+  TextStart:
+    "Julen är snart här och det är en magisk atmosfär. Tomtens förberedelser är många och han varje dag problem stånga. Eftersom du en nisse är, du behöver avhjälpa tomten med allt vad det innebär. Om allt faller väl ut, du sprider glädje vid varje husknut.",
+  InputStart: {
+    Text: "Skriv ditt namn",
+  },
+  BtnStart: {
+    Text: "Börja spela",
+  },
+  backgroundImage: 'URL("Items/BackgroundStart.png")',
+};
+
+let inventory = [];
 let activepage = 0;
 
 const pages = [
@@ -6,67 +21,79 @@ const pages = [
     Text: "Om man inte skickat har, får man oftast det som blev kvar. Milo har glömt skicka sin önskemlista till tomten i år, det skulle han gjort i förrgår. Hjälp Milo så listan tomten når, vilket sätt du föreslår?",
     Btn1: {
       Text: "Slangbella",
+      isCorrect: false,
       nextPage: 1,
     },
     Btn2: {
       Text: "Brevduva",
+      isCorrect: false,
       nextPage: 1,
     },
     Btn3: {
       Text: "Tomtens släde",
+      isCorrect: true,
       nextPage: 1,
     },
-    backgroundImage: 'URL("Items/BackgroundMission1.png")', //Backgrundsbilden för första uppdraget
+    backgroundImage: 'URL("Items/BackgroundMission1.png")',
   },
   {
     Headers: "Uppdrag 2",
     Text: "En bit in i december vi är, och tomten har precis mottagit ditt kuvert. Vi står nu utanför hans hus, vi i luften känna det på pepparkakor bjuds. Vi mot hans renar ler, som visar oss runt i detta vinterkvarter. I gengäld vi tillbaka ge, morötter, glass eller kycklingfilé.",
     Btn1: {
       Text: "Morötter",
+      isCorrect: true,
       nextPage: 2,
     },
     Btn2: {
       Text: "Glass",
+      isCorrect: false,
       nextPage: 2,
     },
     Btn3: {
       Text: "Kycklingfilé",
+      isCorrect: false,
       nextPage: 2,
     },
     backgroundImage: 'URL("Items/BackgroundMission2.png")',
   },
   {
     Headers: "Uppdrag 3",
-    Text: "Hej tomtegubbar, tänd ett ljus och mer jul, att sjunga sånger tycker kören är jättekul! En jultext dem är osäkra på, kan du hjälpa dem att texten återfå",
+    Text: "Hej tomtegubbar, tänd ett ljus och mer jul, att sjunga sånger tycker kören är jättekul! En jultext dem är osäkra på, kan du hjälpa dem att texten återfå?",
     Btn1: {
-      Text: "??",
+      Text: "Mer grisfötter",
+      isCorrect: false,
       nextPage: 3,
     },
     Btn2: {
-      Text: "??",
+      Text: "Mer jul",
+      isCorrect: true,
       nextPage: 3,
     },
     Btn3: {
-      Text: "??",
+      Text: "Mer snö",
+      isCorrect: false,
       nextPage: 3,
     },
-    backgroundImage: 'URL("Items/BackgroundMisson3.png")', //Backgrundsbilden för första uppdraget
+    backgroundImage: 'URL("Items/BackgroundMission3_.png)',
   },
   {
     Headers: "Uppdrag 4",
     Text: "Med snö du kan skapa många ting, du kan med tillbehör sedan göra iordning. Filippa, Valter och Agnes sin snögrubbe rullat, ",
     Btn1: {
       Text: "??",
+      isCorrect: true,
       nextPage: 4,
     },
     Btn2: {
       Text: "??",
+      isCorrect: true,
       nextPage: 4,
     },
     Btn3: {
       Text: "??",
+      isCorrect: true,
       nextPage: 4,
     },
-    backgroundImage: 'URL("Items/BackgroundMission4.png")', //Backgrundsbilden för första uppdraget
+    backgroundImage: 'URL("Items/BackgroundMission4.png")',
   },
 ];
