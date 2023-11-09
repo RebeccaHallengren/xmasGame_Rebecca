95eb6d1c7a777518e13053a9ebd74a82941926c2
Rebecca <rebecca_hallengren@hotmail.com>
2023-11-09 16:19:32 +0100

    add design

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", main);
 
+let inventory = [];
+
 function main() {
   moveOn();
 }
@@ -10,7 +12,7 @@ function moveOn() {
   const Button1 = document.querySelector("#option0");
   const Button2 = document.querySelector("#option1");
   const Button3 = document.querySelector("#option2");
-  const Item1 = document.getElementsByClassName("item0"); //INVENTORY?
+  const Item1 = document.getElementsByClassName("showInventory"); //INVENTORY?
   const body = document.getElementById("body");
 
   const page = pages[activepage]; //Pages ligger i missions, och active page är nu 0.
@@ -24,34 +26,57 @@ function moveOn() {
   body.style.backgroundImage = page.backgroundImage;
 
   Button1.onclick = function () {
+    hideMission();
     checkAnswer(page.Btn1.isCorrect, page.Item1, page.Btn1.nextPage); //true/false
+
     // goToNextpage(page.Btn1.nextPage);
   };
 
   Button2.onclick = function () {
+    hideMission();
     checkAnswer(page.Btn2.isCorrect, page.Item1, page.Btn2.nextPage);
+
     // goToNextpage(page.Btn2.nextPage);
   };
 
   Button3.onclick = function () {
+    hideMission();
     checkAnswer(page.Btn3.isCorrect, page.Item1, page.Btn3.nextPage);
+
     // goToNextpage(page.Btn3.nextPage);
   };
 }
 
-function checkAnswer(isCorrect, item, nextPage) {
+//Funktionen går igenom alla element i hidden och döljer dem.
+function hideMission() {
+  const hidden = document.querySelectorAll(".missionWrapper");
+  hidden.forEach(function (element) {
+    element.style.display = "none";
+  });
+}
+
+function checkAnswer(isCorrect, Item1, nextPage) {
   if (isCorrect === true) {
-    inventory.push(item);
+    inventory.push(Item1);
+    updateInventoryCount();
     showMessageRightAnswer(nextPage);
+
+    return page;
   } else if (isCorrect === false) {
     showMessageWrongAnswer(nextPage);
   }
 }
+
+function updateInventoryCount() {
+  const inventoryCount = document.getElementsByClassName("item0");
+  inventoryCount.textContent = inventory.length;
+}
+
 //Kan man slå ihop right and wrong answer?? If / else
 function showMessageRightAnswer(nextPage) {
   const rightAnswer = document.querySelector(".rightAnswer");
-
-  rightAnswer.style.display = "block";
+  rightAnswer.style.display = "grid";
+  rightAnswer.style.placeItems = "center";
 
   setTimeout(function () {
     rightAnswer.style.display = "none";
@@ -59,15 +84,20 @@ function showMessageRightAnswer(nextPage) {
   }, 3000);
 }
 
+//gå till nästa sida och display element
 function goToNextpage(pageIndex) {
+  const hidden = document.querySelectorAll(".missionWrapper");
+  hidden.forEach(function (element) {
+    element.style.display = "block";
+  });
   activepage = pageIndex;
   moveOn();
 }
 
 function showMessageWrongAnswer(nextPage) {
   const wrongAnswer = document.querySelector(".wrongAnswer");
-
-  wrongAnswer.style.display = "block";
+  wrongAnswer.style.display = "grid";
+  wrongAnswer.style.placeItems = "center";
 
   setTimeout(function () {
     wrongAnswer.style.display = "none";
--- missons.js
+++ missons.js
@@ -12,88 +12,128 @@ const firstPage = {
   backgroundImage: 'URL("Items/BackgroundStart.png")',
 };
 
-let inventory = [];
 let activepage = 0;
 
 const pages = [
   {
-    Headers: "Uppdrag 1",
+    Headers: "Rädda pepparkakorna",
+    Text: "Vid jul man ofta bakar, och tänder sina ljusstakar. Pepparkakor är vid denna tid en trendig deg, men här håller man på att begå ett felsteg. Vilken av ingrediens ska tas bort? ",
+    Btn1: {
+      Text: "Smör",
+      isCorrect: false,
+      nextPage: 1,
+    },
+    Btn2: {
+      Text: "Citron",
+      isCorrect: true,
+      nextPage: 1,
+    },
+    Btn3: {
+      Text: "Kanel",
+      isCorrect: false,
+      nextPage: 1,
+    },
+    backgroundImage: 'URL("Items/gingerbread.jpg")',
+  },
+
+  {
+    Headers: "Julbrevet har kommit bort!",
     Text: "Om man inte skickat har, får man oftast det som blev kvar. Milo har glömt skicka sin önskemlista till tomten i år, det skulle han gjort i förrgår. Hjälp Milo så listan tomten når, vilket sätt du föreslår?",
     Btn1: {
       Text: "Slangbella",
       isCorrect: false,
-      nextPage: 1,
+      nextPage: 2,
     },
     Btn2: {
-      Text: "Brevduva",
+      Text: "Ring Postnord",
       isCorrect: false,
-      nextPage: 1,
+      nextPage: 2,
     },
     Btn3: {
       Text: "Tomtens släde",
       isCorrect: true,
-      nextPage: 1,
+      nextPage: 2,
     },
-    backgroundImage: 'URL("Items/BackgroundMission1.png")',
+    backgroundImage: 'URL("Items/backgroundMailToSanta.png")',
   },
   {
-    Headers: "Uppdrag 2",
-    Text: "En bit in i december vi är, och tomten har precis mottagit ditt kuvert. Vi står nu utanför hans hus, vi i luften känna det på pepparkakor bjuds. Vi mot hans renar ler, som visar oss runt i detta vinterkvarter. I gengäld vi tillbaka ge, morötter, glass eller kycklingfilé.",
+    Headers: "Mata tomtens renar!",
+    Text: "Vi står nu utanför tomtens hus, vi i luften känna det på pepparkakor bjuds. Vi mot hans renar ler, som visar oss runt i detta vinterkvarter. I gengäld vi tillbaka ge, morötter, glass eller kycklingfilé.",
     Btn1: {
       Text: "Morötter",
       isCorrect: true,
-      nextPage: 2,
+      nextPage: 3,
     },
     Btn2: {
       Text: "Glass",
       isCorrect: false,
-      nextPage: 2,
+      nextPage: 3,
     },
     Btn3: {
       Text: "Kycklingfilé",
       isCorrect: false,
-      nextPage: 2,
+      nextPage: 3,
     },
     backgroundImage: 'URL("Items/BackgroundMission2.png")',
   },
   {
-    Headers: "Uppdrag 3",
-    Text: "Hej tomtegubbar, tänd ett ljus och mer jul, att sjunga sånger tycker kören är jättekul! En jultext dem är osäkra på, kan du hjälpa dem att texten återfå?",
+    Headers: "Julkören sprider stämning!",
+    Text: "Hej tomtegubbar och tänd ett ljus, kören sjunger för varje grannhus! En jultext dem är osäkra på, kan du hjälpa dem att texten återfå? 'Jag vill ha ...'",
     Btn1: {
       Text: "Mer grisfötter",
       isCorrect: false,
-      nextPage: 3,
+      nextPage: 4,
     },
     Btn2: {
       Text: "Mer jul",
       isCorrect: true,
-      nextPage: 3,
+      nextPage: 4,
     },
     Btn3: {
       Text: "Mer snö",
       isCorrect: false,
-      nextPage: 3,
+      nextPage: 4,
     },
-    backgroundImage: 'URL("Items/BackgroundMission3_.png)',
+    backgroundImage: 'URL("Items/christmasCarrol.png")',
   },
   {
-    Headers: "Uppdrag 4",
-    Text: "Med snö du kan skapa många ting, du kan med tillbehör sedan göra iordning. Filippa, Valter och Agnes sin snögrubbe rullat, ",
+    Headers: "Ingen jul utan snögubbe!",
+    Text: "Lisa snöbollar rulla, när hon ställt dem på varann blir det väldigt effektfulla. Med morot och pinne hon använder sitt barnasinne. Kan du se vad som saknas på gubben här, eller blir resultatet sisådär?  ",
     Btn1: {
-      Text: "??",
+      Text: "Öga",
       isCorrect: true,
-      nextPage: 4,
+      nextPage: 5,
     },
     Btn2: {
-      Text: "??",
-      isCorrect: true,
-      nextPage: 4,
+      Text: "Fötter",
+      isCorrect: false,
+      nextPage: 5,
     },
     Btn3: {
-      Text: "??",
-      isCorrect: true,
-      nextPage: 4,
+      Text: "Tomteluva",
+      isCorrect: false,
+      nextPage: 5,
     },
     backgroundImage: 'URL("Items/BackgroundMission4.png")',
+  },
+  {
+    Headers: "Hugga gran!",
+    Text: "I varje hem den ofta står och på julafton man runt den går. Man klär den med hoppfulla ljus och glitter, som gör att barnen fylls av fnitter. Hur kan vi få granen hem, ett verktyg behövs så lägg på en rem. ",
+    Btn1: {
+      Text: "Ståltråd",
+      isCorrect: false,
+      nextPage: 5,
+    },
+    Btn2: {
+      Text: "Yxa",
+      isCorrect: true,
+      nextPage: 5,
+    },
+    Btn3: {
+      Text: "Skruvdragare",
+      isCorrect: false,
+      nextPage: 5,
+    },
+    backgroundImage: 'URL("Items/christmasTreeCar.png")',
   },
 ];
